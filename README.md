14779a140ebd985fafd6524e077152e69e153599
Jimmy Larsson <jimmy.w.larsson@gmail.com>
2025-02-24 23:05:00 +0100

    add gui, readme and remove lockfile

---
+++ README.md
@@ -0,0 +1,3 @@
+Compile both the gui and recording to get the functionality
+take the path to the gui from the config file,
+make it check in both ./target/debug/ and ./target/release for the gui program
---
+++ src/bin/gui.rs
@@ -0,0 +1,24 @@
+#![allow(unused, unreachable_code)]
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+use std::io::Read;
+
+fn main() -> Result<()> {
+    let mut buf = [0; 8];
+
+    eframe::run_simple_native(
+        "FreKeyency",
+        eframe::NativeOptions::default(),
+        move |ctx, frame| {
+            let read = std::io::stdin()
+                .read(&mut buf)
+                .expect("failed to read stdin");
+
+            egui::CentralPanel::default().show(ctx, |ui| {
+                let s = format!("{}", String::from_utf8_lossy(&buf[..read]));
+                ui.label(s);
+            });
+        },
+    );
+    Ok(())
+}
