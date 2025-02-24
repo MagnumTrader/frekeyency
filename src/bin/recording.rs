d75d8e0235b796d5cd97798ee8417139dffb5f67
Jimmy Larsson <jimmy.w.larsson@gmail.com>
2025-02-24 23:03:50 +0100

    move recorder to its own program in /bin

--- src/bin/recording.rs
+++ src/bin/recording.rs
@@ -3,15 +3,32 @@
 use evdev::{Device, KeyCode};
 use xkbcommon::xkb::{self, Context, KeyDirection, Keycode};
 
-use std::{fs, thread, time::Duration};
-
-// TODO: chars that can be combined are named DEAD_X, example DEAD_CIRCUMFLEX for ^ 
-// or DEAD_TILDE for ~, this is because they can be combined to create â or ñ, 
+use std::{
+    fs,
+    io::Write,
+    process::{Child, ChildStdin, Stdio},
+    thread,
+    time::Duration,
+};
+// TODO: chars that can be combined are named DEAD_X, example DEAD_CIRCUMFLEX for ^
+// or DEAD_TILDE for ~, this is because they can be combined to create â or ñ,
 // therefore they dont send chars, fix this later by catching the variant for "dead keys"
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 /// char offset to convert from evdev to xkbcommon
 const XKB_OFFSET: u16 = 8;
+
+struct AppChild {
+    child: Child,
+    pipe: ChildStdin,
+}
+
+impl Drop for AppChild {
+    fn drop(&mut self) {
+        self.child.kill().expect("failed to kill child process");
+        self.child.wait().expect("failed to wait for child exit");
+    }
+}
 
 fn main() -> Result<()> {
     // pick a device
@@ -20,7 +37,7 @@ fn main() -> Result<()> {
     // Initialize xkbcommon for key translation
     let context = xkb::Context::new(xkb::CONTEXT_NO_FLAGS);
 
-    // TODO: 
+    // TODO:
     // dump layout and pipe through and load the keymap with std::command.
     // check https://www.youtube.com/watch?v=6jFSr_a_xEQ&t=2337s
     //
@@ -39,11 +56,12 @@ fn main() -> Result<()> {
 
     let mut state = xkb::State::new(&keymap);
 
+    let mut child = spawn_window();
+
     // get all the event and print them.
     'outer: loop {
         for event in dev.fetch_events().expect("failed to get events") {
             if let evdev::EventSummary::Key(event, code, value) = event.destructure() {
-
                 // HACK: Escape is doing something to our state,
                 //       So we remove it for now
                 if code == KeyCode::KEY_ESC {
@@ -64,11 +82,12 @@ fn main() -> Result<()> {
                 if !sym.is_modifier_key() && value == 1 {
                     let x = state.key_get_utf8((code.0 + 8).into());
                     /// this doesnt print '"'
+                    ///
+                    child.pipe.write_all(x.as_bytes());
                     println!("sym {sym:?} you sent: {x}");
                 }
             }
         }
-
         thread::sleep(Duration::from_millis(10));
     }
 
@@ -109,11 +128,25 @@ pub fn pick_device() -> evdev::Device {
     }
 }
 
-fn spawn_window() {
-    // spawn a window for our amazing things
-    //
-    //
-    //
+// HACK: Egui must run i the mainthread and xkbcommon or evdev to work properly, dont remember which
+// therefore we spawn another process instead of using a thread.
+fn spawn_window() -> AppChild {
+    let mut command = std::process::Command::new("./target/debug/gui");
+    let mut child = command
+        .stdin(Stdio::piped())
+        .spawn()
+        .expect("failed to spawn UI process");
 
+    let mut pipe = match child.stdin.take() {
+        Some(pipe) => pipe,
+        None => {
+            // needed to not not leave a ghost process
+            // implemented in the drop function of AppChild
+            child.kill().expect("failed to kill child process");
+            child.wait().expect("failed to await child process");
+            panic!("failed to take standard in from gui process")
+        }
+    };
 
+    AppChild { child, pipe }
 }
